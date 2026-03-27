57ab3cd932bd090714125f38c3d62980e272dd30
Nikita <leonovn70@gmail.com>
2026-03-27 19:49:22 +0000

    1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-def main():
-    print("Bot project started")
+import asyncio
+
+from app.bot.bot import bot, dp
+from app.bot.handlers import routers
+
+
+async def main():
+    # подключаем роутеры
+    for router in routers:
+        dp.include_router(router)
+
+    print("Bot started 🚀")
+
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
-    
+    asyncio.run(main())
